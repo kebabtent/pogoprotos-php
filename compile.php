cac8a3ef78bf29f1149d5542e31bb00b5b829a35
jaspervdm <jaspervdmaarel.nl@gmail.com>
2016-08-01 23:45:26 +0200

    Use protobuf-php/protobuf-plugin for compilation of classes

---
+++ compile.php
@@ -0,0 +1,37 @@
+<?php
+$_IN = __DIR__."/protos/src";
+$_OUT = __DIR__."/src";
+
+function listProtos($dir) {
+  $files = [];
+  if ($handle = opendir($dir)) {
+    while (false !== ($entry = readdir($handle))) {
+      if ($entry != "." && $entry != "..") {
+        if (is_dir($dir."/".$entry)) {
+          $files = array_merge($files, listProtos($dir."/".$entry));
+        }
+        else {
+          $ext = explode(".", $entry);
+          if (array_pop($ext) == "proto") {
+            $files[] = $dir."/".$entry;
+          }
+        }
+      }
+    }
+    closedir($handle);
+  }
+
+  return $files;
+}
+
+$fp = fopen("out.log", "w");
+$files = listProtos($_IN);
+foreach ($files as $file) {
+  $cmd = "php ./vendor/protobuf-php/protobuf-plugin/bin/protobuf -i \"".$_IN."\" -o \"".$_OUT."\" \"".$file."\"";
+  fwrite($fp, "Compile ".$file."\n");
+  $out = shell_exec($cmd);
+  fwrite($fp, $out);
+}
+fclose($fp);
+
+echo "Log written to out.log\n";
